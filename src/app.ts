1d05538b7f365ee30becb13046be684ec1b3d7fc
Tran Anh Hung <>
2021-01-30 16:11:07 +0700

    lesson 15

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,30 @@
 import { Route } from 'core/interfaces';
 import express from 'express';
+import hpp from 'hpp';
+import helmet from 'helmet';
 import mongoose from 'mongoose';
+import morgan from 'morgan';
+import cors from 'cors';
+import { Logger } from './core/utils';
 
 class App {
     public app: express.Application;
     public port: string | number;
+    public production: boolean;
 
     constructor(routes: Route[]){
         this.app = express();
         this.port = process.env.PORT || 5000;
+        this.production = process.env.NODE_ENV == 'production' ? true : false;
 
         this.initializeRoutes(routes);
         this.connectToDatabase();
+        this.initializerMiddleware();
     }
 
     public listen() {
         this.app.listen(this.port, () => {
-            console.log(`Server is listening on port ${this.port}`);
+            Logger.info(`Server is listening on port ${this.port}`);
         });
     }
 
@@ -26,25 +34,33 @@ class App {
         });
     }
 
-    private connectToDatabase() {
-        try {
-            const connectString = process.env.MONGODB_URI;
-            if (!connectString) {
-                console.log('Connect string is invalid');
-                return;
-            }
-            console.log(connectString);
-            mongoose.connect(connectString, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                useFindAndModify: false,
-                useCreateIndex: true
-            });
-            console.log('Database connected...');
-            
-        } catch (error) {
-            console.log('Connect to database error');
+    private initializerMiddleware() {
+        if (this.production) {
+            this.app.use(hpp());
+            this.app.use(helmet());
+            this.app.use(morgan('combined'));
+            this.app.use(cors({origin: 'your.domain.com', credentials: true}))
+        } else {
+            this.app.use(morgan('dev'));
+            this.app.use(cors({origin: true, credentials: true}))
         }
+    }
+
+    private connectToDatabase() {
+        const connectString = process.env.MONGODB_URI;
+        if (!connectString) {
+            Logger.error('Connect string is invalid');
+            return;
+        }
+        mongoose.connect(connectString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        }).catch((err) => {
+            Logger.error(err);
+        });
+        Logger.info('Database connected...');
     }
  }
 
